f1a6ae4c101b8673ee03245319830c186326728b
Baptiste Delhomme <baptdelhomme@gmail.com>
2023-10-09 23:01:38 +0200

    Ajout composant Card

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,33 @@
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: 'https://via.placeholder.com/80',
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+        picture: 'https://via.placeholder.com/80',
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: 'https://via.placeholder.com/80',
+    },
+]
+
 function Freelances() {
     return (
         <div>
-            <h1>Les Freelances sont ici 📸</h1>
+            <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
         </div>
     )
 }
